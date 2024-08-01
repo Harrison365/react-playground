0d9462cb93e8b910fb08b00102dcf30eb8e24eb6
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-08-01 18:55:44 +0100

    added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { useState } from "react";
-import MyComponent from "./MyComponent";
+import Stuff from "./Stuff";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [arr, setArr] = useState([]);
-  const [newEle, setNewEle] = useState("");
-
-  function deleteItem(targetIndex) {
-    setArr((current) => {
-      return current.filter((item, index) => {
-        return targetIndex != index;
-      });
-    });
-  }
-
   return (
-    <div className="yo">
-      <h1>Counter {count}</h1>
-      <button
-        onClick={() =>
-          setCount((currcount) => {
-            return currcount + 1;
-          })
-        }
-      >
-        +
-      </button>
-      <MyComponent2 id="yo" count={count} setCount={setCount} />
-      <MyComponent count={count} setCount={setCount} />
-      {arr.length ? (
-        <ul>
-          {arr.map((item, index) => {
-            return (
-              <div>
-                <li>{item}</li>
-                <button
-                  onClick={(e) => {
-                    deleteItem(index);
-                  }}
-                >
-                  Delete
-                </button>
-              </div>
-            );
-          })}
-        </ul>
-      ) : (
-        <h3>List Empty</h3>
-      )}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (newEle !== "") {
-            //No empty strings ^^^
-            setArr((currArr) => {
-              return [...currArr, newEle];
-            });
-          }
-          setNewEle("");
-        }}
-      >
-        <input
-          value={newEle}
-          onChange={(e) => {
-            setNewEle(e.target.value);
-          }}
-        ></input>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
-}
-
-function MyComponent2({ count, setCount }) {
-  return (
-    <div>
-      <h1>@@@@@@@@@</h1>
-    </div>
+    <>
+      <h1>Always Here</h1>
+      <Routes>
+        <Route path="" element={<Stuff />} />
+        <Route path="/hi" element={<h1>Hi</h1>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Stuff.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { useState } from "react";
+import MyComponent from "./MyComponent";
+
+export default function Stuff() {
+  const [count, setCount] = useState(0);
+  const [arr, setArr] = useState([]);
+  const [newEle, setNewEle] = useState("");
+
+  function deleteItem(targetIndex) {
+    setArr((current) => {
+      return current.filter((item, index) => {
+        return targetIndex != index;
+      });
+    });
+  }
+
+  return (
+    <div className="yo">
+      <h1>Counter {count}</h1>
+      <button
+        onClick={() =>
+          setCount((currcount) => {
+            return currcount + 1;
+          })
+        }
+      >
+        +
+      </button>
+      <MyComponent2 id="yo" count={count} setCount={setCount} />
+      <MyComponent count={count} setCount={setCount} />
+      {arr.length ? (
+        <ul>
+          {arr.map((item, index) => {
+            return (
+              <div>
+                <li>{item}</li>
+                <button
+                  onClick={(e) => {
+                    deleteItem(index);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </ul>
+      ) : (
+        <h3>List Empty</h3>
+      )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (newEle !== "") {
+            //No empty strings ^^^
+            setArr((currArr) => {
+              return [...currArr, newEle];
+            });
+          }
+          setNewEle("");
+        }}
+      >
+        <input
+          value={newEle}
+          onChange={(e) => {
+            setNewEle(e.target.value);
+          }}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+function MyComponent2({ count, setCount }) {
+  return (
+    <div>
+      <h1>@@@@@@@@@</h1>
+    </div>
+  );
+}
